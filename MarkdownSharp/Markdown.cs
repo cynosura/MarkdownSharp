304d8bddacb1d9544b0827ad883002611c63e239
theanti9 <the.anti.9@gmail.com>
2010-05-18 20:46:12 -0400

    first commit

---
+++ MarkdownSharp/Markdown.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MarkdownSharp
+{
+	public class Markdown
+	{
+		private string InputText;
+		private int place;
+
+		public Markdown(string input)
+		{
+			InputText = input;
+			place = 0;
+		}
+
+		public string Parse()
+		{
+			string OutputString = InputText;
+
+			OutputString = ParseLinks(OutputString);
+
+			return OutputString;
+		}
+
+		private string ParseLinks(string input) {
+
+		}
+
+		private string ReadToEndOfLine(int startindex, string input)
+		{
+			StringBuilder sb = new StringBuilder();
+			int i = startindex;
+			while (input[i] != '\r' && input[i] != '\n')
+			{
+				sb.Append(input[i]);
+				i++;
+			}
+			return sb.ToString();
+		}
+
+		private string ReadToCharacter(char c, int startindex, string input)
+		{
+			StringBuilder sb = new StringBuilder();
+			int i = startindex;
+			while (input[i] != c)
+			{
+				sb.Append(input[i]);
+				i++;
+			}
+			return sb.ToString();
+		}
+	}
+}
