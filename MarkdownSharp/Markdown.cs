a8181897ba3d0cfd12afb1391640d555c7e046a4
theanti9 <the.anti.9@gmail.com>
2010-05-23 18:41:39 -0400

    Added basic link parsing

--- MarkdownSharp/Markdown.cs
+++ MarkdownSharp/Markdown.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -26,7 +26,37 @@ namespace MarkdownSharp
 		}
 
 		private string ParseLinks(string input) {
-
+			for (int i = 0; i < input.Length; i++) {
+				if (input[i] == '[' && input[i-1] != '\\') {
+					int start = i;
+					string linktext = ReadToCharacter(']', i+1, input);
+					i+= linktext.Length+2;
+					if (input[i] == '(') {
+						string parencontents = ReadToCharacter(')', i+1, input);
+						if (parencontents.IndexOf(' ') > -1) {
+							string[] spl = parencontents.Split(new char[] {' '}, 1);
+							string url = spl[0];
+							string title = spl[1];
+						} else {
+							string url = parencontents;
+							string title = null;
+						}
+						int len = linktext.Length+2+parenscontent+2;
+						// to be replaced
+						string tbr = input.Substring(start, len);
+						// to replace with
+						string trw ="<a href=\"" + url +"\"";
+						if (title != null) {
+							trw += " title=" + title;
+						}
+						trw += ">" + linktext + "</a>";
+						input.Replace(tbr, trw);
+						i+=len;
+					} else {
+						// Link is a reference
+					}
+				}
+			}
 		}
 
 		private string ReadToEndOfLine(int startindex, string input)
@@ -35,8 +65,12 @@ namespace MarkdownSharp
 			int i = startindex;
 			while (input[i] != '\r' && input[i] != '\n')
 			{
-				sb.Append(input[i]);
-				i++;
+				try {
+					sb.Append(input[i]);
+					i++;
+				} catch (Exception) {
+					break;
+				}
 			}
 			return sb.ToString();
 		}
@@ -47,8 +81,12 @@ namespace MarkdownSharp
 			int i = startindex;
 			while (input[i] != c)
 			{
-				sb.Append(input[i]);
-				i++;
+				try {
+					sb.Append(input[i]);
+					i++;
+				} catch (Exception) {
+					break;
+				}
 			}
 			return sb.ToString();
 		}
