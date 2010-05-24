f35515515180ba51ac2185fc949679762be9a98d
theanti9 <the.anti.9@gmail.com>
2010-05-23 23:47:05 -0400

    Finished link parsing.

--- MarkdownSharp/Markdown.cs
+++ MarkdownSharp/Markdown.cs
@@ -1,62 +1,117 @@
 using System;
+using System.Collections;
+
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace MarkdownSharp
 {
 	public class Markdown
 	{
 		private string InputText;
-		private int place;
-
+		private Hashtable LinkReferences;
 		public Markdown(string input)
 		{
 			InputText = input;
-			place = 0;
 		}
 
 		public string Parse()
 		{
 			string OutputString = InputText;
-
+			LinkReferences = new Hashtable();
+			GenLinkReferenceTable();
 			OutputString = ParseLinks(OutputString);
 
 			return OutputString;
 		}
 
-		private string ParseLinks(string input) {
-			for (int i = 0; i < input.Length; i++) {
-				if (input[i] == '[' && input[i-1] != '\\') {
+		private void GenLinkReferenceTable()
+		{
+			Regex rx = new Regex(@"\[([a-zA-Z0-9_-]+)\]: (.*?) ("".*?"")?");
+			MatchCollection matches = rx.Matches(InputText);
+			foreach (Match match in matches)
+			{
+				GroupCollection groups = match.Groups;
+				string title = null;
+				if (groups.Count > 2) {
+					title = groups[3].Value;
+				}
+				LinkReferences.Add(groups[1].Value, new LinkReference(groups[2].Value, title));
+			}
+		}
+
+		private string ParseLinks(string input)
+		{
+			for (int i = 0; i < input.Length; i++)
+			{
+				if (input[i] == '[' && input[i - 1] != '\\')
+				{
 					int start = i;
-					string linktext = ReadToCharacter(']', i+1, input);
-					i+= linktext.Length+2;
-					if (input[i] == '(') {
-						string parencontents = ReadToCharacter(')', i+1, input);
-						if (parencontents.IndexOf(' ') > -1) {
-							string[] spl = parencontents.Split(new char[] {' '}, 1);
-							string url = spl[0];
-							string title = spl[1];
-						} else {
-							string url = parencontents;
-							string title = null;
+					string linktext = ReadToCharacter(']', i + 1, input);
+					i += linktext.Length + 2;
+					if (input[i] == '(')
+					{
+						string parencontents = ReadToCharacter(')', i + 1, input);
+						string url;
+						string title;
+						if (parencontents.IndexOf(' ') > -1)
+						{
+							string[] spl = parencontents.Split(new char[] { ' ' }, 1);
+							url = spl[0];
+							title = spl[1];
 						}
-						int len = linktext.Length+2+parenscontent+2;
+						else
+						{
+							url = parencontents;
+							title = null;
+						}
+						int len = linktext.Length + 2 + parencontents.Length + 2;
 						// to be replaced
 						string tbr = input.Substring(start, len);
 						// to replace with
-						string trw ="<a href=\"" + url +"\"";
-						if (title != null) {
+						string trw = "<a href=\"" + url + "\"";
+						if (title != null)
+						{
 							trw += " title=" + title;
 						}
 						trw += ">" + linktext + "</a>";
-						input.Replace(tbr, trw);
-						i+=len;
-					} else {
-						// Link is a reference
+						input = input.Replace(tbr, trw);
+						i += len;
+					}
+					else if (input[i] == '[' || (input[i] == ' ' && input[i + 1] == '['))
+					{
+						bool hasspace = false;
+						int refidstart = i + 1;
+						if (input[i] == ' ')
+						{
+							hasspace = true;
+							start++;
+						}
+						string refid = ReadToCharacter(']', refidstart, input);
+						LinkReference link = (LinkReference)LinkReferences[refid];
+						int len = linktext.Length + 2 + refid.Length + 2;
+						if (hasspace)
+						{
+							len++;
+						}
+
+						// to be replaced
+						string tbr = input.Substring(start, len);
+						// to replace with
+						string trw = "<a href=\"" + link.Url + "\"";
+						if (link.Title != null)
+						{
+							trw += " title=" + link.Title;
+						}
+						trw += ">" + linktext + "</a>";
+						input = input.Replace(tbr, trw);
+						i += len;
 					}
 				}
 			}
+			return input;
 		}
 
 		private string ReadToEndOfLine(int startindex, string input)
@@ -65,10 +120,13 @@ namespace MarkdownSharp
 			int i = startindex;
 			while (input[i] != '\r' && input[i] != '\n')
 			{
-				try {
+				try
+				{
 					sb.Append(input[i]);
 					i++;
-				} catch (Exception) {
+				}
+				catch (Exception)
+				{
 					break;
 				}
 			}
@@ -81,14 +139,29 @@ namespace MarkdownSharp
 			int i = startindex;
 			while (input[i] != c)
 			{
-				try {
+				try
+				{
 					sb.Append(input[i]);
 					i++;
-				} catch (Exception) {
+				}
+				catch (Exception)
+				{
 					break;
 				}
 			}
 			return sb.ToString();
+		}
+	}
+
+	public class LinkReference
+	{
+		public string Url;
+		public string Title;
+
+		public LinkReference(string url, string title)
+		{
+			Url = url;
+			Title = title;
 		}
 	}
 }
