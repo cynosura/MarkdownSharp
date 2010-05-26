1c324fcfbb7acedf8f30d0b6a34c0358a57c4d3f
theanti9 <the.anti.9@gmail.com>
2010-05-25 20:27:12 -0400

    added emphasis parsing. beginning of inline code parsing

--- MarkdownSharp/Markdown.cs
+++ MarkdownSharp/Markdown.cs
@@ -23,20 +23,27 @@ namespace MarkdownSharp
 			LinkReferences = new Hashtable();
 			GenLinkReferenceTable();
 			OutputString = ParseLinks(OutputString);
+			OutputString = ParseEmphasis(OutputString);
+			OutputString = ParseInlineCode(OutputString);
 
 			return OutputString;
 		}
 
 		private void GenLinkReferenceTable()
 		{
-			Regex rx = new Regex(@"\[([a-zA-Z0-9_-]+)\]: (.*?) ("".*?"")?");
+			Regex rx = new Regex(@"\[([a-zA-Z0-9_-]+)\]: (\S+)\s*([""|'].*?[""|'])?");
 			MatchCollection matches = rx.Matches(InputText);
 			foreach (Match match in matches)
 			{
 				GroupCollection groups = match.Groups;
 				string title = null;
-				if (groups.Count > 2) {
+				try
+				{
 					title = groups[3].Value;
+				}
+				catch (Exception)
+				{
+					// keep title null
 				}
 				LinkReferences.Add(groups[1].Value, new LinkReference(groups[2].Value, title));
 			}
@@ -58,7 +65,7 @@ namespace MarkdownSharp
 						string title;
 						if (parencontents.IndexOf(' ') > -1)
 						{
-							string[] spl = parencontents.Split(new char[] { ' ' }, 1);
+							string[] spl = parencontents.Split(new char[] { ' ' }, 2);
 							url = spl[0];
 							title = spl[1];
 						}
@@ -67,9 +74,9 @@ namespace MarkdownSharp
 							url = parencontents;
 							title = null;
 						}
-						int len = linktext.Length + 2 + parencontents.Length + 2;
+						int len = linktext.Length + 2 + parencontents.Length + 3;
 						// to be replaced
-						string tbr = input.Substring(start, len);
+						string tbr = input.Substring(start - 1, len);
 						// to replace with
 						string trw = "<a href=\"" + url + "\"";
 						if (title != null)
@@ -87,18 +94,18 @@ namespace MarkdownSharp
 						if (input[i] == ' ')
 						{
 							hasspace = true;
-							start++;
+							refidstart += 1;
 						}
 						string refid = ReadToCharacter(']', refidstart, input);
 						LinkReference link = (LinkReference)LinkReferences[refid];
 						int len = linktext.Length + 2 + refid.Length + 2;
 						if (hasspace)
 						{
-							len++;
+							len += 1;
 						}
-
+						len += 1;
 						// to be replaced
-						string tbr = input.Substring(start, len);
+						string tbr = input.Substring(start - 1, len);
 						// to replace with
 						string trw = "<a href=\"" + link.Url + "\"";
 						if (link.Title != null)
@@ -109,6 +116,88 @@ namespace MarkdownSharp
 						input = input.Replace(tbr, trw);
 						i += len;
 					}
+					else if (input[i] == ':')
+					{
+						string str = ReadToEndOfLine(start, input);
+						input = input.Replace(str, "");
+					}
+				}
+			}
+			return input;
+		}
+
+		private string ParseEmphasis(string input)
+		{
+			for (int i = 0; i < input.Length; i++)
+			{
+				if (input[i] == '*' && input[i - 1] != '\\' && !(input[i-1] == ' ' && input[i+1] == ' '))
+				{
+					if (input[i + 1] == '*')
+					{
+						string str = ReadToCharacter('*', i + 2, input);
+						while (input[i + str.Length + 2] != '*')
+						{
+							str += ReadToCharacter('*', i + str.Length + 3, input);
+						}
+						input = input.Replace("**" + str + "**", "<strong>" + str + "</strong>");
+						i += str.Length + 17;
+					}
+					else
+					{
+						string str = ReadToCharacter('*', i + 1, input);
+						input = input.Replace("*" + str + "*", "<em>" + str + "</em>");
+						i += str.Length + 9;
+					}
+				}
+				else if (input[i] == '_' && input[i - 1] != '\\' && !(input[i - 1] == ' ' && input[i + 1] == ' '))
+				{
+					if (input[i + 1] == '_')
+					{
+						string str = ReadToCharacter('_', i + 2, input);
+						while (input[i + str.Length + 3] != '_')
+						{
+							str += ReadToCharacter('_', i + str.Length + 3, input);
+						}
+						input = input.Replace("__" + str + "__", "<strong>" + str + "</strong>");
+						i += str.Length + 17;
+					}
+					else
+					{
+						string str = ReadToCharacter('_', i + 1, input);
+						input = input.Replace("_" + str + "_", "<em>" + str + "</em>");
+						i += str.Length + 9;
+					}
+				}
+			}
+			return input;
+		}
+
+		private string ParseInlineCode(string input)
+		{
+			for (int i = 0; i < input.Length; i++)
+			{
+				if (input[i] == '`' && input[i - 1] != '\\')
+				{
+					if (input[i + 1] == '`')
+					{
+						string str = ReadToCharacter('`', i + 2, input);
+						while (input[i + str.Length + 3] != '`')
+						{
+							str += ReadToCharacter('`', i + str.Length + 3, input);
+						}
+						string tbr = "``" + str + "``";
+						str = str.Replace("&", "&amp;");
+						str = str.Replace("<", "&lt;");
+						str = str.Replace(">", "&gt;");
+						input = input.Replace(tbr, "<code>" + str + "</code>");
+						i += str.Length + 13;
+					}
+					else
+					{
+						string str = ReadToCharacter('`', i + 1, input);
+						input = input.Replace("`" + str + "`", "<code>" + str + "</code>");
+						i += str.Length + 13;
+					}
 				}
 			}
 			return input;
@@ -118,17 +207,17 @@ namespace MarkdownSharp
 		{
 			StringBuilder sb = new StringBuilder();
 			int i = startindex;
-			while (input[i] != '\r' && input[i] != '\n')
+			try
 			{
-				try
+				while (input[i] != '\r' && input[i] != '\n')
 				{
 					sb.Append(input[i]);
 					i++;
 				}
-				catch (Exception)
-				{
-					break;
-				}
+			}
+			catch (Exception)
+			{
+				// return
 			}
 			return sb.ToString();
 		}
@@ -137,17 +226,17 @@ namespace MarkdownSharp
 		{
 			StringBuilder sb = new StringBuilder();
 			int i = startindex;
-			while (input[i] != c)
+			try
 			{
-				try
+				while (input[i] != c)
 				{
 					sb.Append(input[i]);
 					i++;
 				}
-				catch (Exception)
-				{
-					break;
-				}
+			}
+			catch (Exception)
+			{
+				// return
 			}
 			return sb.ToString();
 		}
--- MarkdownSharpTester/test.md
+++ MarkdownSharpTester/test.md
@@ -14,11 +14,6 @@ This is [an example](http://example.com/ "Title") inline link.
 [This link](http://example.net/) has no title attribute.
 Will produce:
 
-<p>This is <a href="http://example.com/" title="Title">
-an example</a> inline link.</p>
-
-<p><a href="http://example.net/">This link</a> has no
-title attribute.</p>
 If you’re referring to a local resource on the same server, you can use relative paths:
 
 See my [About](/about/) page for details.   
@@ -32,3 +27,50 @@ This is [an example] [id] reference-style link.
 Then, anywhere in the document, you define your link label like this, on a line by itself:
 
 [id]: http://example.com/  "Optional Title Here"
+
+EMPHASIS
+
+*single asterisks*
+
+_single underscores_
+
+**double asterisks**
+
+__double underscores__
+
+You can use whichever style you prefer; the lone restriction is that the same character must be used to open and close an emphasis span.
+
+Emphasis can be used in the middle of a word:
+
+un*frigging*believable
+But if you surround an * or _ with spaces, it’ll be treated as a literal asterisk or underscore.
+
+To produce a literal asterisk or underscore at a position where it would otherwise be used as an emphasis delimiter, you can backslash escape it:
+
+\*this text is surrounded by literal asterisks\*
+
+CODE
+
+To indicate a span of code, wrap it with backtick quotes (\`). Unlike a pre-formatted code block, a code span indicates code within a normal paragraph. For example:
+
+Use the `printf()` function.
+will produce:
+
+To include a literal backtick character within a code span, you can use multiple backticks as the opening and closing delimiters:
+
+``There is a literal backtick (`) here.``
+
+The backtick delimiters surrounding a code span may include spaces — one after the opening, one before the closing. This allows you to place literal backtick characters at the beginning or end of a code span:
+
+A single backtick in a code span: `` ` ``
+
+A backtick-delimited string in a code span: `` `foo` ``
+
+With a code span, ampersands and angle brackets are encoded as HTML entities automatically, which makes it easy to include example HTML tags. Markdown will turn this:
+
+Please don't use any `<blink>` tags.
+into:
+
+
+`&#8212;` is the decimal-encoded equivalent of `&mdash;`.
+to produce:
